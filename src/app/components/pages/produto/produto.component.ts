049728c00d73780df31ca173ec85099740748021
André Montanari <montanari.andre@aluno.ifsp.edu.br>
2022-07-06 00:11:33 -0300

    Estilização e Funcionalidades página Produtos

--- src/app/components/pages/produto/produto.component.ts
+++ src/app/components/pages/produto/produto.component.ts
@@ -11,42 +11,75 @@ import { ListProdutosService } from 'src/app/services/list-produtos.service';
 })
 export class ProdutoComponent implements OnInit {
 
-  produto?:  Produto[]
+  produtosCategoriaUm ?:  Produto[]
+  produtosOutros?:  Produto[]
+  produto?:  Produto
 
   public id = this.route.snapshot.paramMap.get('id')
-
-  public categoria = "Comédia"
-
-
-  //produto?:  Produto
 
   constructor(
     private listService : ListProdutosService, 
     private route : ActivatedRoute
     ) {
+
       this.getProduto()
-      
+      this.getCategoria()
+      this.getOutros()
+
      }
 
   ngOnInit(): void {
-  }
+    
+  } 
 
- /*
-  getProduto(){//Carregar da API apenas um item
+  getProduto() {//Carregar da API apenas um item
     const id = this.route.snapshot.paramMap.get("id")
-    console.log('VALOR DE ID '+id)
-    if(id){      
-
-      this.listService.getItem(id).subscribe((produto) => (this.produto = produto))
-      
-    }
+    console.log('VALOR DE ID ' + id)
+    if (id) {
+      this.listService.getItem(id).subscribe((produto) => (
+        this.produto = produto
+        ))
+    }   
+   
   }
-  */
+
+  getProdutoLoad(id:any) {//Carregar da API apenas um item
+   
+    console.log('VALOR DE ID ' + id)
+    if (id) {
+      this.listService.getItem(id).subscribe((produto) => (
+        this.produto = produto
+        ))
+    }   
+   
+  }
+
+  getCategoria(){
+    const id = this.route.snapshot.paramMap.get("id")
+    console.log('VALOR DE ID ' + id)
+    if (id) {
+      this.listService.getItem(id).subscribe((produto) => (
+         //console.log('categoria é ' + produto.categoria?.slice(0,1))
+          this.getPrimeiraCategoria(produto.categoria?.slice(0,1))
+        ))
+    }   
+  }
+
+  getPrimeiraCategoria(categoria:any){
+    return this.listService.getCategoria(categoria).subscribe((produto) => (this.produtosCategoriaUm = produto));
+  }
+
+  getOutros(){
+    return this.listService.getAll().subscribe((produto) => (this.produtosOutros = produto.sort().reverse().slice(0,15)));
+  }
+//.slice(0,1)
+//
+
+
 
   
-  getProduto(){      
-      return this.listService.getAll().subscribe((produto) => (this.produto = produto)); 
-  }
-  
+
+ 
+
 
 }
